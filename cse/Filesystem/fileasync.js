da5039748fde4d2544c835cf2c8145ce91cbb1fd
atharva0773 <atharvasingh0072@gmail>
2025-02-20 21:18:54 +0530

    commited

--- cse/Filesystem/fileasync.js
+++ cse/Filesystem/fileasync.js
@@ -1,15 +1,15 @@
-const fspromise= require('fs').promises;
+const fspromise = require('fs').promises;
 
-const read=async()=>{
-    try{
-        const data= await fspromise.readFile("./data.txt","utf-8");
-        console.log(data)
-
-    }
-    catch(error){
+const read = async () => {
+    try {
+        const data = await fspromise.readFile("./data.txt", "utf-8", ()=>{
+            if(err) console.log(err.message);
+        });
+        console.log(data);
+    } catch (error) {
         console.log(error);
     }
-   
-}
+};
+
 read();
 console.log("1st");
